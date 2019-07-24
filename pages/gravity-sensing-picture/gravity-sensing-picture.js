536f98ee707084e724baff856dfcaed34db9cbe9
chenLong <1182927503@qq.com>
2019-07-24 14:36:18 +0800

    重力感应页面

--- pages/gravity-sensing-picture/gravity-sensing-picture.js
+++ pages/gravity-sensing-picture/gravity-sensing-picture.js
@@ -4,54 +4,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-    startBtnState: false,
-    endBtnState: true,
-    y: -50
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
+    
   },
 
-  start: function() {
-    let _this = this
-    wx.startGyroscope({
-      interval: 'ui',
-      success(res) {
-        _this.setData({
-          startBtnState: true,
-          endBtnState: false
-        })
-        wx.onGyroscopeChange(function (res) {
-          let rx = res.x
-          // 小程序宽度最大为100%
-          // 只有当变化超过阀值
-          if (Math.abs(rx) > 0.1) {
-            let y = _this.data.y
-            y += rx * 2
-            // console.log(y)
-            if (y < 0 && y > -100) {
-              _this.setData({
-                y
-              })
-            }
-          }
-        })
-      }
-    })
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
   },
 
-  end: function() {
-    let _this = this
-    wx.stopGyroscope({
-      success(res) {
-        _this.setData({
-          startBtnState: false,
-          endBtnState: true
-        })
-      }
-    })
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
