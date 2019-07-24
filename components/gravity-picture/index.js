22cc6574602af254f6d75fa981dddaa2b6cf21aa
chenLong <1182927503@qq.com>
2019-07-24 11:57:39 +0800

    重力感应图片
    自定义垂直方向可移动范围

--- components/gravity-picture/index.js
+++ components/gravity-picture/index.js
@@ -3,14 +3,15 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    src: String
+    src: String,
+    y: Number
   },
 
   /**
  * 组件的初始数据
  */
   data: {
-    y: -50
+    minY: this.data.y * 2
   },
 
   // 外部样式
@@ -21,7 +22,7 @@ Component({
     detached: function () {
       // 在组件实例被从页面节点树移除时执行
       wx.stopGyroscope({
-        success(res) { }
+        success(res) {}
       })
     },
   },
@@ -34,14 +35,14 @@ Component({
       wx.startGyroscope({
         interval: 'ui',
         success(res) {
+          let minY = _this.data.minY
           wx.onGyroscopeChange(function (res) {
             let rx = res.x
             // 只有当变化超过阀值
             if (Math.abs(rx) > 0.1) {
               let y = _this.data.y
               y += rx * 2
-              // console.log(y)
-              if (y < 0 && y > -100) {
+              if (y < 0 && y > minY) {
                 _this.setData({
                   y
                 })
@@ -54,15 +55,8 @@ Component({
     // 页面隐藏
     hide: function () {
       wx.stopGyroscope({
-        success(res) { }
+        success(res) {}
       })
     }
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
   }
 })
