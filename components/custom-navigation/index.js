533b6f046ebcdd026972c85d2012fcd29d645162
chenLong <1182927503@qq.com>
2019-08-08 17:01:12 +0800

    自定义导航栏
    修改样式

--- components/custom-navigation/index.js
+++ components/custom-navigation/index.js
@@ -1,6 +1,5 @@
 const app = getApp()
 let globalData = app.globalData
-console.log(globalData)
 Component({
   /**
    * 组件的属性列表
@@ -25,22 +24,30 @@ Component({
     share: globalData.share,
     statusBarHeight: globalData.statusBarHeight,
     navigationHeight: globalData.navigationHeight
-  }, 
+  },
+
+  lifetimes: {
+    attached: function() {
+      this.setData({
+        share: app.globalData.share
+      })
+    },
+  },
 
   /**
    * 组件的方法列表
    */
   methods: {
     // 返回
-    back: function(){
+    back: function() {
       let backLevel = this.data.backLevel
       wx.navigateBack({
         delta: backLevel
       })
     },
     // 回到首页
-    home: function(){
-      wx.reLaunch({
+    home: function() {
+      wx.switchTab({
         url: '/pages/index/index'
       })
     }
